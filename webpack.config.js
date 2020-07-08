97db92f21894932221087dd13f0cf38521f8e736
Daniel Howitt <daniel.howittcontact@gmail.com>
2020-07-08 14:53:01 +0100

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: path.resolve(__dirname, "./lib/wagon_race.js"),
+  entry: path.resolve(__dirname, "./lib/something.js"),
   mode: "development",
   output: {
     path: path.resolve(__dirname, "dist"),
